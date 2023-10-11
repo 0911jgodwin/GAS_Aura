15fe6ad77b0f8ab405c3ba711262a04c2a20a216
0911jgodwin <exogenesis96@gmail.com>
2023-10-11 15:45:13 +0100

    Aura user widget and widget controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright JG
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright JG
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
