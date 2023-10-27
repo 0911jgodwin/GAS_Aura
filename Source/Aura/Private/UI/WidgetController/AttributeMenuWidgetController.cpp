ecec57b47cf1da3737812a42eada8d94fa51e557
0911jgodwin <exogenesis96@gmail.com>
2023-10-27 16:43:47 +0100

    Implemented attribute menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright JG
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
