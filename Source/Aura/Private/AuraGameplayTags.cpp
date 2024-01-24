8ae655975f39e0bbea7794ef9b31eb9386571a26
0911jgodwin <exogenesis96@gmail.com>
2024-01-24 16:11:06 +0000

    Tidied up firebolt ability and set Shamans to be able to use that to attack. Moved summon to a summon ability tag

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -192,6 +192,11 @@ void FAuraGameplayTags::InitialiseNativeGameplayTags()
 		FName("Abilities.Attack"),
 		FString("Attack Ability Tag")
 		);
+
+	GameplayTags.Abilities_Summon = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Summon"),
+		FString("Summon Ability Tag")
+		);
 	
 	/*
 	 * Combat Sockets
