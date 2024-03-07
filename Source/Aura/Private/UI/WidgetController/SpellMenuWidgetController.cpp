cb608dcdab2800c1f9674e95d346d36a4d67d54d
0911jgodwin <exogenesis96@gmail.com>
2024-03-07 15:41:44 +0000

    Setting up spell menu widget controller

---
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright JG
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
