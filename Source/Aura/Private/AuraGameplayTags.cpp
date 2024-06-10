3b44020eddddad3908ba9e96eccfc8ccd28e7c52
0911jgodwin <exogenesis96@gmail.com>
2024-06-10 22:56:18 +0100

    Added basic fireblast skill info

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -277,6 +277,11 @@ void FAuraGameplayTags::InitialiseNativeGameplayTags()
 		FString("FireBolt Ability Tag")
 		);
 
+	GameplayTags.Abilities_Fire_FireBlast = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Fire.FireBlast"),
+		FString("FireBlast Ability Tag")
+		);
+
 	GameplayTags.Abilities_Lightning_Electrocute = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Lightning.Electrocute"),
 		FString("Electrocute Ability Tag")
